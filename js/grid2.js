9537307372eb0cd4fe9b15d5a66c898da9819e4e
Manuel Serra <manuelserranunes@gmail.com>
2018-03-16 14:33:38 +0000

    layout da informação dos partners corrigido

--- js/grid2.js
+++ js/grid2.js
@@ -191,7 +191,7 @@ var Grid = (function() {
 		support = Modernizr.csstransitions,
 		// default settings
 		settings = {
-			minHeight : 250,
+			minHeight : 350,
 			speed : 350,
 			easing : 'ease'
 		};
@@ -361,9 +361,12 @@ var Grid = (function() {
 		create : function() {
 			// create Preview structure:
 			this.$title = $( '<h3></h3>' );
-			this.$description = $( '<p></p>' );
+            this.$text = $( '<p></p>' );
+            this.$textwrap = $( '<div class="og-textwrap"></div>' ).append(this.$text);
+			this.$description = $( '<div class="og-description"></div>' ).append(this.$textwrap);
 			this.$href = $( '<a href="#">Visit website</a>' );
-			this.$details = $( '<div class="og-details"></div>' ).append( this.$title, this.$description, this.$href );
+
+			this.$details = $( '<div class="og-details"></div>' ).append( /*this.$title ,*/ this.$description, this.$href );
 			this.$loading = $( '<div class="og-loading"></div>' );
 			this.$fullimage = $( '<div class="og-fullimg"></div>' ).append( this.$loading );
 			this.$closePreview = $( '<span class="og-close"></span>' );
@@ -405,7 +408,7 @@ var Grid = (function() {
 				};
 
 			this.$title.html( eldata.title );
-			this.$description.html( eldata.description );
+			this.$textwrap.html( eldata.description );
 			this.$href.attr( 'href', eldata.href );
 
 			var self = this;
